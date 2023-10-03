dbc58ac9aba21005102ac163ec561bece30b31fb
CherryRain <117277361+cherryrainOuO@users.noreply.github.com>
2023-10-03 16:34:25 +0900

    주 프롬프트
    완료 목록
    1. 키 입력
    2. 각 키에 해당하는 프롬프트로 이동
    3. 오류 메세지 출력
    4. 종료 프롬프트
    
    해야하는 목록
    1. 일정 분류 프롬프트
    2. 검색 프롬프트
    3. 일정 관리 프롬프트
    4. 나머지 클래스 들

--- KU_Introductory_Project_2-B17/Main.cpp
+++ KU_Introductory_Project_2-B17/Main.cpp
@@ -1,1 +1,91 @@
-#include "Main.h"
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+void MainPrompt();
+void ClassificationPrompt();
+void SearchPrompt();
+void MangementPrompt();
+void ExitPrompt();
+
+int main() {
+	
+	while (true) {
+
+		MainPrompt();
+	}
+
+	
+	
+
+	return 0;
+}
+
+void MainPrompt() {
+	Sleep(100);
+	system("cls");
+
+	cout << "캘린더 홈\n\n";
+
+	cout << "1. 일정 분류\n";
+	cout << "2. 검색\n";
+	cout << "3. 일정 관리\n";
+	cout << "4. 종료\n\n";
+
+	cout << "원하는 번호를 입력하세요.\n";
+	cout << "-------------------------------------\n";
+	cout << ">";
+
+	string n;
+	getline(cin, n);
+
+	switch (n[0])
+	{
+	case '1':
+		ClassificationPrompt();
+		break;
+	case '2':
+		SearchPrompt();
+		break;
+	case '3':
+		MangementPrompt();
+		break;
+	case '4':
+		ExitPrompt();
+		break;
+	default:
+		system("cls");
+
+		cout << "오류 : 1, 2, 3, 4 중 하나의 숫자를 입력해주세요.\n\n";
+		cout << "아무 키나 눌러주세요.\n";
+		cout << "-------------------------------------\n";
+		cout << ">";
+		
+		_getch();
+
+		break;
+	}
+
+}
+
+void ClassificationPrompt() {
+	system("cls");
+}
+
+void SearchPrompt() {
+	system("cls");
+}
+
+void MangementPrompt() {
+	system("cls");
+}
+
+void ExitPrompt() {
+	system("cls");
+	cout << "캘린더 앱을 종료합니다.\n";
+	exit(0);
+}
+
