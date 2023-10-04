6f8aabdc50d623da07d841eae829cd8e8fed29ad
lylylylh <nameisyh02@konkuk.ac.kr>
2023-10-04 21:26:04 +0900

    management 클래스

--- KU_Introductory_Project_2-B17/Main.cpp
+++ KU_Introductory_Project_2-B17/Main.cpp
@@ -6,6 +6,7 @@
 
 #include "Classification.h"
 #include "Calender.h"
+#include "Management.h"
 using namespace std;
 
 void MainPrompt();
@@ -17,11 +18,12 @@ void ExitPrompt();
 Calender calender;
 KeywordSearch ks(&calender); //test
 Classification classification;
+Management mng(&calender);
 
 int main() {
 
 	// 임의 데이터 (나중에 지울거임)
-	Schedule s1 = Schedule("s1", "2023/04/28", "2023/05/02","c1", "m1");
+	Schedule s1 = Schedule("s1", "2023/04/28", "2023/05/02", "c1", "m1");
 	Schedule s2 = Schedule("s2", "2023/04/29", "2023/04/29", "c2", "m2");
 	Schedule s3 = Schedule("s3", "2023/04/29", "2023/04/29", "c3", "m3");
 	Schedule s4 = Schedule("s4", "2023/04/30", "2023/05/06", "c4", "m4");
@@ -69,7 +71,7 @@ void MainPrompt() {
 		ks.prompt(); // 검색
 		break;
 	case '3':
-		ManagementPrompt(); // 일정 관리
+		mng.Prompt(); // 일정 관리
 		break;
 	case '4':
 		ExitPrompt(); // 종료
@@ -97,113 +99,11 @@ void ClassificationPrompt() {
 void SearchPrompt() {
 	system("cls");
 }
-*/
 
 void ManagementPrompt() {
 	system("cls");
-	string dateinfo; // yyyy/mm/dd
-	string y, m, d; // 각각 year, month, date
-	bool b = false; // dateinfo가 입력 형식에 맞는지 확인하기 위한 변수
-
-	cout << "날짜 입력\n\n";
-
-	cout << "입력 형식 : yyyy/mm/dd\n";
-	cout << "(^C 입력 시 이전 화면으로 돌아갑니다)\n\n";
-
-	cout << "원하는 날짜를 입력하세요.\n";
-	cout << "------------------------------------\n";
-	cout << "> ";
-
-	//! cin >> dateinfo;
-	getline(cin, dateinfo); // cin 말고 getline을 사용.
-
-	if (dateinfo == "^C") // 취소 명령어
-		MainPrompt();
-	else {
-		if (dateinfo.length() == 10 && dateinfo[4] == '/' && dateinfo[7] == '/'){ 
-			y = dateinfo.substr(0, 4);
-			m = dateinfo.substr(5, 2);
-			d = dateinfo.substr(8, 2);
-			
-			if (stoi(y) >= 2000 && stoi(y) <= 2030
-				&& stoi(m)>=1&& stoi(m)<=12&&stoi(d)>=1) {
-				
-				switch (stoi(m)) {
-				
-				case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-					if (stoi(d) <= 31)
-						b = true;
-					break;
-				
-				case 4: case 6: case 9: case 11: 
-					if (stoi(d) <= 30)
-						b = true;
-					break;
-				
-				case 2:
-					if (stoi(y) % 4) {
-						if (stoi(d) <= 28)
-							b = true;
-					}
-					else {
-						if (stoi(d) <= 29)
-							b = true;
-					}
-					break;
-				}
-			}
-		}
-		if(!b) {
-			system("cls");
-			cout << "오류: 날짜를 형식에 맞게 입력해주세요.\n\n";
-
-			cout << "아무 키나 눌러주세요.\n";
-			cout << "_____________________________\n";
-			cout << "> ";
-			_getch();
-		}
-		else {
-			system("cls");
-			string sd, ed;  // startDate, endDate
-			vector<Schedule> sche; // 현재 날짜에 해당하는 스케줄
-			
-			for (int i = 0; i < calender.allSchs.size(); i++) {			
-				sd = calender.allSchs[i].getStartDate();
-				ed = calender.allSchs[i].getEndDate();
-				
-				if (dateinfo >= sd && dateinfo <= ed) 
-					sche.push_back(calender.allSchs[i]);
-			}
-			if (sche.size() > 0) {
-				cout << "<" << dateinfo << "의 일정>\n\n";
-				
-				// 사용자가 입력한 날짜의 일정 목록
-				for (int i = 0; i < sche.size(); i++)
-					sche[i].print();
-
-				cout << "1. 일정 추가\n";
-				cout << "2. 일정 수정/삭제\n\n";
-
-				cout << "(^C 입력 시 이전 화면으로 돌아갑니다)\n\n";
-				cout << "원하는 메뉴를 입력하세요.\n";
-				cout << "------------------------------\n";
-				cout << "> ";
-				_getch();
-			}
-
-			else { // 날짜에 해당하는 스케줄이 존재하지 않음
-				cout << "<(사용자가 입력한 날짜)의 일정>\n\n";
-				cout << "일정이 없습니다.일정을 추가하세요.\n\n";
-				cout << "1. 일정 추가\n\n";
-				cout << "(^ C 입력 시 이전 화면으로 돌아갑니다)\n\n";
-				cout << "원하는 메뉴를 입력하세요\n";
-				cout << "-------------------------------\n";
-				cout << "> ";
-				_getch();
-			}
-		}
-	}
 }
+*/
 
 void ExitPrompt() {
 	system("cls");
