381eaf0f62a8eda52e20f48d7c9494447d1cf0a4
lylylylh <nameisyh02@konkuk.ac.kr>
2023-10-05 20:32:49 +0900

    데이터 로드

--- KU_Introductory_Project_2-B17/Main.cpp
+++ KU_Introductory_Project_2-B17/Main.cpp
@@ -31,19 +31,6 @@ int main() {
 	CDM.loadDataFile(category);
 
 	Sleep(2000);
-	// 임의 데이터 (나중에 지울거임)
-	Schedule s1 = Schedule("s1", "2023/04/28", "2023/05/02", "기본", "m1");
-	Schedule s2 = Schedule("s2", "2023/04/29", "2023/04/29", "기본", "m2");
-	Schedule s3 = Schedule("s3", "2023/04/29", "2023/04/29", "카테1", "m3");
-	Schedule s4 = Schedule("s4", "2023/04/30", "2023/05/06", "카테2", "m4");
-	Schedule s5 = Schedule("s5", "2023/04/30", "2023/04/30", "카테3", "m5");
-
-	calender.allSchs.push_back(s1);
-	calender.allSchs.push_back(s2);
-	calender.allSchs.push_back(s3);
-	calender.allSchs.push_back(s4);
-	calender.allSchs.push_back(s5);
-	
 
 	while (true) {
 
