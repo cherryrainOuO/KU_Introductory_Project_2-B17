3d301c24a3f14270e445f048f79ed6176c1677ca
yeon <onu3303@gmail.com>
2023-11-25 14:01:12 +0900

    Update Schedule.h
    카테고리 vector로 변경 및 대체 일정 구분용 키 추가

--- KU_Introductory_Project_2-B17/Schedule.h
+++ KU_Introductory_Project_2-B17/Schedule.h
@@ -10,17 +10,17 @@ private:
 	string title;
 	string startDate;
 	string endDate;
-	string category;
+	vector<string> category;
 	string memo;
 	int key; 
 	int cycle; //0: x, 1: 매년, 2: 매달, 3: 매주
 	string rptEndDate; //반복종료일
-
+	int rptK; //대체 일정 구분
 	int rc = 0; // 레퍼런스 카운트 : 일정분류에서 카테고리 연산에 따른 여러 일정 출력할 때 중복 체크 용도입니다.
 
 public:
 	Schedule();
-	Schedule(string t, string sd, string ed, string ca, string me, string rED, int cy, int k);
+	Schedule(string t, string sd, string ed, vector<string> ca, string me, string rED, int cy, int k, int rptK);
 	void print();
 	//getter & setter
 	string getTitle() { return title; };
@@ -29,8 +29,8 @@ public:
 	void setStartDate(string date) { startDate = date; };
 	string getEndDate() { return endDate; };
 	void setEndDate(string date) { endDate = date; };
-	string getCategory() { return category; };
-	void setCategory(string cat) { category = cat; };
+	vector<string> getCategory() { return category; };
+	void setCategory(vector<string> cat) { category = cat; };
 	string getMemo() { return memo; };
 	void setMemo(string mm) { memo = mm; };
 	int getKey() { return key; };
